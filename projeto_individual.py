e8e28c49f88498e9d0e948b79498b22ce5d816e6
Diegool97 <113364521+Diegool97@users.noreply.github.com>
2024-04-05 10:45:39 -0300

    Add files via upload
    Continuação

--- projeto_individual.py
+++ projeto_individual.py
@@ -8,25 +8,48 @@ candidatos = [
 ]
 
 #OUTROS RESULTADOS O MESMO FORMATO
-nome_candidato = input(f'''
-===========================================
-        Informe o nome do candito:
-        => ''')
-entrevista = input(f'''
+entrevista = int(input(f'''
 ===========================================
         Informe a avaliação da entrevista(e):
-        => ''')
-teste_teorico = input(f'''
+        => '''))
+teste_teorico = int(input(f'''
 ===========================================
         Informe a avaliação do teste teórico(t):
-        => ''')
-teste_pratico = input(f'''
+        => '''))
+teste_pratico = int(input(f'''
 ===========================================
         Informe a avaliação do teste prático(p):
-        => ''')
-soft_skills = input(f'''
+        => '''))
+soft_skills = int(input(f'''
 ===========================================
         Informe a avaliação do soft skills(s):
-        => ''')
+        => '''))
 
 # FUNÇÃO QUE BUSCA O CANDIDATO DE ACORDO COM OS CRITÉRIOS DIGITADOS
+def busca_candidato(candidatos, nota_minima_entrevista, nota_minima_teste_teorico, nota_minima_teste_pratico, nota_minima_soft_skills):
+    candidatos_compativeis = []
+    for candidato in candidatos:
+        notas = candidato[1].split('_')  # DIVISÃO DA STRING EM PARTES PARA ESTRAÇÃO
+        nota_entrevista = int(notas[0][1:])  
+        nota_teste_teorico = int(notas[1][1:])  
+        nota_teste_pratico = int(notas[2][1:])  
+        nota_soft_skills = int(notas[3][1:])  
+        if (nota_entrevista >= entrevista and
+            nota_teste_teorico >= teste_teorico and
+            nota_teste_pratico >= teste_pratico and
+            nota_soft_skills >= soft_skills):
+            candidatos_compativeis.append(candidato)
+    return candidatos_compativeis
+
+candidatos_encontrados = busca_candidato(candidatos, entrevista, teste_teorico, teste_pratico, soft_skills)
+
+# EXIBIÇÃO DO RESULTADO
+if candidatos_encontrados:
+    print("\nCandidatos compatíveis encontrados:")
+    for candidato in candidatos_encontrados:
+        print(f"Nome: {candidato[0]}")
+else:
+    print("\nNenhum candidato compatível encontrado com os critérios especificados.")
+
+
+
